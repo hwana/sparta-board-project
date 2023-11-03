0d288e6b2a4e25169125b9a32ab5cea9c6ee3073
hwanhee Kim <dsa1461@gmail.com>
2023-11-03 16:30:57 +0900

    first commit

---
+++ src/test/java/com/sparta/boardproject/SpartaBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
