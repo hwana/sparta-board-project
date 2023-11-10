9ea1a051b7fafb0ba3191279bcfd5ef0b458ef5b
hwanhee Kim <dsa1461@gmail.com>
2023-11-10 12:19:23 +0900

    :adhesive_bandage: chore: import 정리 및 어노테이션 변경

--- src/main/java/com/sparta/boardproject/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/sparta/boardproject/common/exception/CustomExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.sparta.boardproject.common.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e){
